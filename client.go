1e018adfe2f9b9b4209ae72da4f74ced1bce1a04
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2015-04-20 16:35:21 +0200

    One Goroutine for each connection

--- client.go
+++ client.go
@@ -12,7 +12,7 @@ import (
 	"time"
 
 	"./socks"
-	"./verbose"
+	//"./verbose"
 )
 
 const (
@@ -57,7 +57,6 @@ func NewForwardProxy(listenAddr string, revProxAddr string, tickIntervalMsec int
 }
 
 func handleSocksConnection(conn net.Conn) {
-	verbose.TSPrintf("socks connect from %s\n", conn.RemoteAddr().String())
 	closed := false
 	defer func() {
 		if !closed {
@@ -94,27 +93,32 @@ func handleSocksConnection(conn net.Conn) {
 		}
 	}()
 
-	go PipeThenClose(conn, remote, NO_TIMEOUT)
-	PipeThenClose(remote, conn, NO_TIMEOUT)
+	//go PipeThenClose(conn, remote, NO_TIMEOUT)
+	//PipeThenClose(remote, conn, NO_TIMEOUT)
 
 	closed = true
 	log.Println("closed connection to", addr)
 }
 
-func (f *ForwardProxy) ListenAndServe() error {
+func (f *ForwardProxy) ListenAndServe() {
 	listener, err := net.Listen("tcp", f.listenAddr)
 	if err != nil {
+		log.Fatal(err)
 		panic(err)
 	}
 	log.Printf("listen on '%v', with revProxAddr '%v'", f.listenAddr, f.revProxyAddr)
 
-	conn, err := listener.Accept()
-	if err != nil {
-		panic(err)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("accept:", err)
+			continue
+		}
+		log.Println("accept with", conn.LocalAddr(), "from ", conn.RemoteAddr())
+		go handleSocksConnection(conn)
 	}
-	log.Println("accept conn", "localAddr.", conn.LocalAddr(), "remoteAddr.", conn.RemoteAddr())
 
-	handleSocksConnection(conn)
+	// --------------- NOT REACHED
 
 	buf := new(bytes.Buffer)
 
@@ -127,7 +131,8 @@ func (f *ForwardProxy) ListenAndServe() error {
 	if err != nil {
 		panic(err)
 	}
-	key, err := ioutil.ReadAll(resp.Body)
+	key, err := ioutil.ReadAll(resp.Body) // key, err
+	log.Println(key)
 	if err != nil {
 		panic(err)
 	}
@@ -137,51 +142,10 @@ func (f *ForwardProxy) ListenAndServe() error {
 
 	// ticker to set a streaming rate
 	tick := time.NewTicker(time.Duration(int64(f.tickIntervalMsec)) * time.Millisecond)
+	log.Println(tick)
 
 	//read := makeReadChan(conn, bufSize)
-	buf.Reset()
-	sendCount := 0
-	for {
-		select {
-			/*
-		case b := <-read:
-			// fill buf here
-			log.Println(len(b))
-			//verbose.TSPrintf("client: <-read of '%s'; hex:'%x' of length %d added to buffer\n", string(b), b, len(b))
-			//buf.Write(b)
-			//verbose.TSPrintf("client: after write to buf of len(b)=%d, buf is now length %d\n", len(b), buf.Len())
-*/
-		case <-tick.C:
-			sendCount++
-			verbose.Log("\n ====================\n client sendCount = %d\n ====================\n", sendCount)
-			verbose.Log("client: sendCount %d, got tick.C. key as always(?) = '%x'. buf is now size %d\n", sendCount, key, buf.Len())
-			// write buf to new http request, starting with key
-			req := bytes.NewBuffer(key)
-			buf.WriteTo(req)
-			resp, err := http.Post(
-				"http://"+f.revProxyAddr+"/ping",
-				"application/octet-stream",
-				req)
-			if err != nil && err != io.EOF {
-				log.Println(err.Error())
-				continue
-			}
-
-			// Write http response response to conn
-			// we take apart the io.Copy to print out the response for debugging.
-			//_, err = io.Copy(conn, resp.Body)
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				panic(err)
-			}
-			verbose.Log("client: resp.Body = '%s'\n", string(body))
-			_, err = conn.Write(body)
-			if err != nil {
-				panic(err)
-			}
-			resp.Body.Close()
-		}
-	}
+	//buf.Reset()
 }
 
 func main() {
--- pipe.go
+++ pipe.go
@@ -46,3 +46,49 @@ func PipeThenClose(src, dst net.Conn, timeoutOpt int) {
 		}
 	}
 }
+
+
+/*
+
+	sendCount := 0
+	for {
+		select {
+		case b := <-read:
+			// fill buf here
+			log.Println(len(b))
+			//verbose.TSPrintf("client: <-read of '%s'; hex:'%x' of length %d added to buffer\n", string(b), b, len(b))
+			//buf.Write(b)
+			//verbose.TSPrintf("client: after write to buf of len(b)=%d, buf is now length %d\n", len(b), buf.Len())
+		case <-tick.C:
+			sendCount++
+			verbose.Log("\n ====================\n client sendCount = %d\n ====================\n", sendCount)
+			verbose.Log("client: sendCount %d, got tick.C. key as always(?) = '%x'. buf is now size %d\n", sendCount, key, buf.Len())
+			// write buf to new http request, starting with key
+			req := bytes.NewBuffer(key)
+			buf.WriteTo(req)
+			resp, err := http.Post(
+				"http://"+f.revProxyAddr+"/ping",
+				"application/octet-stream",
+				req)
+			if err != nil && err != io.EOF {
+				log.Println(err.Error())
+				continue
+			}
+
+			// Write http response response to conn
+			// we take apart the io.Copy to print out the response for debugging.
+			//_, err = io.Copy(conn, resp.Body)
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				panic(err)
+			}
+			verbose.Log("client: resp.Body = '%s'\n", string(body))
+			_, err = conn.Write(body)
+			if err != nil {
+				panic(err)
+			}
+			resp.Body.Close()
+		}
+	}
+
+*/
